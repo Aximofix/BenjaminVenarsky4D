99d85cb7b04dfd87aa548a791edcffc990e3ff7e
Benjamín Veňarský <BenjaminVenarsky@s.zochova.sk>
2025-09-11 10:12:56 +0200

    Final

--- Projekt/Controllers/UlohyController.cs
+++ Projekt/Controllers/UlohyController.cs
@@ -56,5 +56,49 @@ namespace Projekt.Controllers
             };
             return View(users);
         }
+
+        public IActionResult Uloha5()
+        {
+            var users = new List<UserInfo>() {
+                new UserInfo() {
+                    Name = "Alex",
+                    LastName = "Vyšný",
+                    Email = "robertopekny@gmail.com",
+                    Phone = "0944 156 153",
+                },
+                new UserInfo() {
+                    Name = "Benjamín",
+                    LastName = "Veňarský",
+                    Email = "bve357@gmail.com",
+                    Phone = "0944 573 179",
+                },
+                new UserInfo() {
+                    Name = "Ján",
+                    LastName = "Novák",
+                    Email = "jan.novak@example.com",
+                    Phone = "0946 456 564",
+                },
+                new UserInfo() {
+                    Name = "Jana",
+                    LastName = "Kováčová",
+                    Email = "jana.kovacova@example.com",
+                    Phone = "0956 412 323",
+                },
+                new UserInfo() {
+                    Name = "Miro",
+                    LastName = "Pavlík",
+                    Email = "miro.pavlik@example.com",
+                    Phone = "0984 856 155",
+                }
+                ,
+                new UserInfo() {
+                    Name = "Urban",
+                    LastName = "Tupý",
+                    Email = "urban.tupy@example.com",
+                    Phone = "0534 512 153",
+                }
+            };
+            return View(users);
+        }
     }
 }
