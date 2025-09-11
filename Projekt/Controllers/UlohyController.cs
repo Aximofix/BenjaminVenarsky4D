fe174af407dae1e8616b3ed93bec169c3521bb7b
Benjamín Veňarský <BenjaminVenarsky@s.zochova.sk>
2025-09-11 09:40:24 +0200

    Uloha4

--- Projekt/Controllers/UlohyController.cs
+++ Projekt/Controllers/UlohyController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Projekt.Models;
 
 namespace Projekt.Controllers
 {
@@ -17,6 +18,43 @@ namespace Projekt.Controllers
         public IActionResult Uloha3()
         {
             return View();
+        }
+
+        public IActionResult Uloha4()
+        {
+            var users = new List<UserInfo>() {
+                new UserInfo() {
+                    Name = "Alex",
+                    LastName = "Vyšný",
+                    Email = "robertopekny@gmail.com",
+                    Phone = "0944 156 153",
+                },
+                new UserInfo() {
+                    Name = "Benjamín",
+                    LastName = "Veňarský",
+                    Email = "bve357@gmail.com",
+                    Phone = "0944 573 179",
+                },
+                new UserInfo() {
+                    Name = "Ján",
+                    LastName = "Novák",
+                    Email = "jan.novak@example.com",
+                    Phone = "0946 456 564",
+                },
+                new UserInfo() {
+                    Name = "Jana",
+                    LastName = "Kováčová",
+                    Email = "jana.kovacova@example.com",
+                    Phone = "0956 412 323",
+                },
+                new UserInfo() {
+                    Name = "Miro",
+                    LastName = "Pavlík",
+                    Email = "miro.pavlik@example.com",
+                    Phone = "0984 856 155",
+                }
+            };
+            return View(users);
         }
     }
 }
