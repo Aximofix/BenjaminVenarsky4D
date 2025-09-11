8c00e3309721376c5f4dc8bff5244adae7b41be8
Benjamín Veňarský <BenjaminVenarsky@s.zochova.sk>
2025-09-11 08:05:20 +0200

    Uloha1

---
+++ Projekt/Controllers/UlohyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projekt.Controllers
+{
+    public class UlohyController : Controller
+    {
+        public IActionResult Uloha1()
+        {
+            return View();
+        }
+    }
+}
